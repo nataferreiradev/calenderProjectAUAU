91f6643e3f1c0aac176b8399db2febbb3bd5a2c4
NataNogueiraFerreira <nataferreira929@hotmail.com>
2024-05-01 20:57:36 -0300

    add insert category

--- lib/modules/controllers/categoryController.py
+++ lib/modules/controllers/categoryController.py
@@ -1,3 +1,6 @@
+from ..models.category import category
+from ..database.immediate import Immediate
+
 class CategoryController:
   TABLENAME = 'category'
   IDCATEGORY = 'idCategory'
@@ -5,3 +8,12 @@ class CategoryController:
   COLOR = 'color'
   STATS = 'stats'
   IDUSER = 'idUser'
+  
+  def insertCategory(category: category):
+    sql = f"""insert into {CategoryController.TABLENAME}({CategoryController.DESCRIPTION},{CategoryController.COLOR},{CategoryController.STATS},{CategoryController.IDUSER})
+              values('{category.description}',{category.COLOR},'{category.stats}',{category.idUser});
+          """
+          
+    resp = Immediate.execute(sql)
+    if resp:
+      print(resp)
